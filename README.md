8c890604e1b5fcec269590cfc6d17251388014f3
Theo <thetheof@gmail.com>
2021-03-17 11:17:14 +0200

    Added a simple example

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # smartclide-broker
+
+A MOM (Message-orientated Middleware) implementation for the communication of the SmartCLIDE components.
---
+++ example/src/test/java/com/demo/springbootrabbitmqexample/SpringBootRabbitmqExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springbootrabbitmqexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRabbitmqExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
